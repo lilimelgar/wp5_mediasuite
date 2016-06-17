c8200b87dc4485d5440750efbb576d73ae05ce70
marijnkoolen <mkoolen@beeldengeluid.nl>
2016-06-17 16:15:04 +0200

    Update collection API show_stats endpoint

--- src/static/js/api/collectionAPI.js
+++ src/static/js/api/collectionAPI.js
@@ -1,7 +1,7 @@
 var collectionAPI = {
 
 	getCollectionStats :function(collectionId, callback) {
-	    var url = _config.SEARCH_API_BASE + "/show_stats/" + collectionId;
+	    var url = _config.SEARCH_API_BASE + "/collections/show_stats?collectionId=" + collectionId;
 	    d3.json(url, function(error, data) {
 	        callback(data);
 	    });
