48b6626163ca24e7a93d09f95edb2a2c2541b339
Jaap Blom <jblom@beeldengeluid.nl>
2016-08-31 16:17:18 +0200

    added explanation on how to override the default

--- src/settings-example.py
+++ src/settings-example.py
@@ -3,5 +3,14 @@ config = {
 	'API_HOST' : '0.0.0.0',
 	'API_PORT' : 5302,
 
-	'INSTANCE_NAME' : 'YOUR_CUSTOM_DIR' #dir should be placed inside templates/custom/
+	"""
+	to override the default, it's possible to create a dir with this instance name in:
+
+	src/templates
+	src/resources/recipes
+	src/static/images
+
+	"""
+
+	'INSTANCE_NAME' : 'YOUR_INSTANCE_NAME'
 }
