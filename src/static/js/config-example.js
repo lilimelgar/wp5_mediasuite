9cf0393ef42ef8684fd4660b213a527b251881cf
Jaap Blom <jblom@beeldengeluid.nl>
2016-09-06 08:03:59 +0200

    removed config.js from the github

---
+++ src/static/js/config-example.js
@@ -0,0 +1,4 @@
+_config = {
+	SEARCH_API_BASE : 'http://localhost:5320/api/v1',
+	ANNOTATION_API_BASE : 'http://localhost:5301/api'
+}
--- src/static/js/config.js
+++
@@ -1,5 +0,0 @@
-_config = {
-	SEARCH_API_BASE : 'http://blofeld.beeldengeluid.nl:5320/api/v1',
-	//ANNOTATION_API_BASE : 'http://blofeld.beeldengeluid.nl:5303/api'
-	ANNOTATION_API_BASE : 'http://localhost:5301/api'
-}
